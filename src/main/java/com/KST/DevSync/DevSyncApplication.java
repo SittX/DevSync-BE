0549706072dce3dbdc651270e3e0eae6bcd76322
Kellot <kellot@example.com>
2024-08-24 16:10:43 +0630

    Initial commit

---
+++ src/main/java/com/KST/DevSync/DevSyncApplication.java
@@ -0,0 +1,13 @@
+package com.KST.DevSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KST/DevSync/DevSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KST.DevSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
